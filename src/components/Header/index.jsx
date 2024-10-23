75dcf12907567e87f59a5172888e4b0635a53a76
David Horès <hores.d@gmail.com>
2024-10-23 16:04:41 +0200

    Create Header SCSS Module stylesheet, add it to Header, create mixins and variables SCSS stylesheets and add them to Header stylesheet

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,42 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 const Header = () => {
     return (
-        <header>
+        <header className={styles.header}>
             <h1>
-                <Link to="/david-hores-portfolio">David Horès</Link>
+                <Link
+                    to="/david-hores-portfolio"
+                    className={styles.header__title}
+                >
+                    David Horès
+                </Link>
             </h1>
             <nav>
-                <ul>
+                <ul className={styles.header__list}>
                     <li>
-                        <Link to="/david-hores-portfolio/realisations">
+                        <Link
+                            to="/david-hores-portfolio/realisations"
+                            className={styles.header__link}
+                        >
                             Réalisations
                         </Link>
                     </li>
                     <li>
-                        <Link to="/david-hores-portfolio/competences">
+                        <Link
+                            to="/david-hores-portfolio/competences"
+                            className={styles.header__link}
+                        >
                             Compétences
                         </Link>
                     </li>
                     <li>
-                        <Link to="/david-hores-portfolio/contact">Contact</Link>
+                        <Link
+                            to="/david-hores-portfolio/contact"
+                            className={styles.header__link}
+                        >
+                            Contact
+                        </Link>
                     </li>
                 </ul>
             </nav>
