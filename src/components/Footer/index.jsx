3d1de57aac5915ec5662f7506aebd3e3e6f6e60e
David Horès <hores.d@gmail.com>
2024-10-23 16:42:53 +0200

    Create Footer SCSS Module stylesheet and add it to Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <p>© 2024 David Horès. Tous droits réservés</p>
         </footer>
     );
