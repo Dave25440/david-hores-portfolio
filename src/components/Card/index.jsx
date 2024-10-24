33809fea7f923ff000edd6e63cd156ddbbb13a80
David Horès <hores.d@gmail.com>
2024-10-24 22:53:15 +0200

    Add Link element to Card and update Card style

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 import styles from "./Card.module.scss";
 
-const Card = ({ title, description }) => {
+const Card = ({ id, title, description }) => {
     return (
-        <article className={styles.card}>
-            <h3 className={styles.card__title}>{title}</h3>
-            <p className={styles.card__description}>{description}</p>
-        </article>
+        <Link
+            to={`/david-hores-portfolio/realisations/${id}`}
+            className={styles.card}
+        >
+            <article>
+                <h3 className={styles.card__title}>{title}</h3>
+                <p className={styles.card__description}>{description}</p>
+            </article>
+        </Link>
     );
 };
 
