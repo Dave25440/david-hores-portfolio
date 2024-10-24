ea42fd9b2aa82c1458718a36cf1318f59fc71b8a
David Horès <hores.d@gmail.com>
2024-10-24 22:57:15 +0200

    Add Card to Home and update Home style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,18 +12,34 @@ const Home = () => {
         <>
             <About />
             <section className={styles.home}>
-                <div>
-                    <h2 className={styles.home__title}>Répondre aux besoins</h2>
-                    <p className={styles.home__introduction}>
-                        En tant que Développeur Web, je transforme vos idées en
-                        solutions techniques afin de répondre à vos besoins et à
-                        ceux de vos utilisateurs.
-                    </p>
+                <div className={styles.home__top}>
+                    <div className={styles.home__introduction}>
+                        <h2 className={styles.home__title}>
+                            Répondre aux besoins
+                        </h2>
+                        <p className={styles.home__text}>
+                            En tant que Développeur Web, je transforme vos idées
+                            en solutions techniques afin de répondre à vos
+                            besoins et à ceux de vos utilisateurs.
+                        </p>
+                    </div>
+                    <Card
+                        id="p5-kasa"
+                        title="Kasa"
+                        description="Location d'appartements entre particuliers"
+                    />
                 </div>
-                <Card
-                    title="Formation"
-                    description="Développeur Web OpenClassrooms (Niveau 5)"
-                />
+                <div className={styles.home__bottom}>
+                    <Card
+                        id="formation"
+                        title="Formation"
+                        description={[
+                            "Développeur Web OpenClassrooms",
+                            <br />,
+                            "Niveau 5"
+                        ]}
+                    />
+                </div>
             </section>
         </>
     );
