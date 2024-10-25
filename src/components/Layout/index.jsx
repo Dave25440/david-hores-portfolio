fd7c37c3619b6d632617d2c54a2423c08b0c4a9e
David Horès <hores.d@gmail.com>
2024-10-25 12:22:01 +0200

    Remove About from Home and add it to Layout with useLocation hook

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,29 @@
+import { useLocation } from "react-router-dom";
 import styles from "./Layout.module.scss";
 import Header from "../Header";
+import About from "../About";
 import Footer from "../Footer";
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+    const paths = [
+        "/david-hores-portfolio",
+        "/david-hores-portfolio/realisations",
+        "/david-hores-portfolio/competences",
+        "/david-hores-portfolio/contact",
+        "/david-hores-portfolio/formation"
+    ];
+
+    const currentPath = location.pathname.replace(/\/$/, "");
+    const isMatch = paths.includes(currentPath);
+
     return (
         <>
             <Header />
-            <main className={styles.main}>{children}</main>
+            <main className={isMatch && styles.main}>
+                {isMatch && <About />}
+                {children}
+            </main>
             <Footer />
         </>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
-import About from "../../components/About";
 import Project from "../../components/Project";
 import Degree from "../../components/Degree";
 import Skill from "../../components/Skill";
@@ -19,37 +18,32 @@ const Home = () => {
     ];
 
     return (
-        <>
-            <About />
-            <section className={styles.home}>
-                <div className={styles.home__top}>
-                    <div className={styles.home__introduction}>
-                        <h2 className={styles.home__title}>
-                            Répondre aux besoins
-                        </h2>
-                        <p className={styles.home__text}>
-                            En tant que Développeur Web, je transforme vos idées
-                            en solutions techniques afin de répondre à vos
-                            besoins et à ceux de vos utilisateurs.
-                        </p>
-                    </div>
-                    <Project
-                        id="p5-kasa"
-                        title="Kasa"
-                        description="Location d'appartements entre particuliers"
-                    />
+        <section className={styles.home}>
+            <div className={styles.home__top}>
+                <div className={styles.home__introduction}>
+                    <h2 className={styles.home__title}>Répondre aux besoins</h2>
+                    <p className={styles.home__text}>
+                        En tant que Développeur Web, je transforme vos idées en
+                        solutions techniques afin de répondre à vos besoins et à
+                        ceux de vos utilisateurs.
+                    </p>
                 </div>
-                <div className={styles.home__bottom}>
-                    <Degree />
-                    <Link
-                        to="/david-hores-portfolio/competences"
-                        className={styles.home__skill}
-                    >
-                        <Skill title="MERN Stack" tools={tools} />
-                    </Link>
-                </div>
-            </section>
-        </>
+                <Project
+                    id="p5-kasa"
+                    title="Kasa"
+                    description="Location d'appartements entre particuliers"
+                />
+            </div>
+            <div className={styles.home__bottom}>
+                <Degree />
+                <Link
+                    to="/david-hores-portfolio/competences"
+                    className={styles.home__skill}
+                >
+                    <Skill title="MERN Stack" tools={tools} />
+                </Link>
+            </div>
+        </section>
     );
 };
 
