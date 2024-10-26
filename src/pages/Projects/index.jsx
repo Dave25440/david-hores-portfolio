e8d16de981f5f47925af5d43a1967f269cab02da
David Horès <hores.d@gmail.com>
2024-10-27 00:48:58 +0200

    Create projects and tools JSON files and add them to Projects and Skills respectively

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,68 +1,12 @@
 import { useEffect } from "react";
 import styles from "./Projects.module.scss";
+import projects from "../../data/projects.json";
 import Project from "../../components/Project";
 
 const Projects = () => {
     useEffect(() => {
         document.title = "David Horès, Développeur Web - Réalisations";
     }, []);
-
-    const projects = [
-        {
-            id: "p2-booki",
-            title: "Booki",
-            subtitle: "Hébergements et activités en ville ou en pleine nature",
-            logo: "Logo",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            order: 3
-        },
-        {
-            id: "p3-sophie-bluel",
-            title: "Sophie Bluel",
-            subtitle: "Site portfolio d'architecte d'intérieur",
-            logo: "Logo",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            order: 5
-        },
-        {
-            id: "p4-nina-carducci",
-            title: "Nina Carducci",
-            subtitle: "Site vitrine de photographe",
-            logo: "Logo",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            order: 2
-        },
-        {
-            id: "p5-kasa",
-            title: "Kasa",
-            subtitle: "Location d'appartements entre particuliers",
-            logo: "Logo",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            order: 1
-        },
-        {
-            id: "p6-mon-vieux-grimoire",
-            title: "Mon Vieux Grimoire",
-            subtitle: "Référencement et notation de livres",
-            logo: "Logo",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            order: 4
-        },
-        {
-            id: "p7-menu-maker-by-qwenta",
-            title: "Menu Maker by Qwenta",
-            subtitle: "Affichage et mise en page de menus pour restaurateurs",
-            logo: "Logo",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            order: 6
-        }
-    ];
 
     return (
         <section className={styles.projects}>
