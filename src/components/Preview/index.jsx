148b788dac64d30a288b666fba57245bd53ecfec
David Horès <hores.d@gmail.com>
2024-10-30 16:20:33 +0100

    Add dialog to Preview with useRef hook and update Preview style

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,24 +1,58 @@
+import { useRef } from "react";
 import styles from "./Preview.module.scss";
 
 const getImage = (image) => {
     try {
         return require(`../../assets/${image}`);
-    } catch {
+    } catch (error) {
+        console.error(error.message);
         return "null.jpg";
     }
 };
 
 const Preview = ({ picture, title, subtitle }) => {
+    const dialogRef = useRef(null);
+    const openModal = () => dialogRef.current && dialogRef.current.showModal();
+    const closeModal = () => dialogRef.current && dialogRef.current.close();
+
     const src = getImage(picture);
 
     return (
-        <figure className={styles.preview}>
-            <img
-                src={src}
-                alt={`${title} : ${subtitle}`}
-                className={styles.preview__img}
-            />
-        </figure>
+        <>
+            <button
+                aria-label={`Voir ${title} : ${subtitle}`}
+                className={styles.preview}
+                onClick={openModal}
+            >
+                <img
+                    src={src}
+                    alt={`${title} : ${subtitle}`}
+                    className={styles.preview__img}
+                />
+            </button>
+            <dialog
+                aria-label={`Aperçu de ${title} : ${subtitle}`}
+                className={styles.preview__dialog}
+                ref={dialogRef}
+                onClick={closeModal}
+            >
+                <button
+                    aria-label="Fermer l'aperçu"
+                    className={styles.preview__button}
+                    onClick={closeModal}
+                >
+                    X
+                </button>
+                <img
+                    src={src}
+                    alt={`${title} : ${subtitle}`}
+                    className={styles.preview__img}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                />
+            </dialog>
+        </>
     );
 };
 
