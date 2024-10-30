93d9db7244560625d6ad59b99c1645d10d91f299
David Horès <hores.d@gmail.com>
2024-10-30 11:42:23 +0100

    Update Skill, add it to Project and update Project and projects data

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -1,19 +1,27 @@
 import styles from "./Skill.module.scss";
 import Tool from "../Tool";
 
-const Skill = ({ title, tools, column }) => {
+const Skill = ({ title, tools, column, skills }) => {
     return (
         <article className={styles.skill}>
             <h3 className={styles.skill__title}>{title}</h3>
-            <ul
-                className={`${styles.skill__tools} ${
-                    column && styles["skill__tools--column"]
-                }`}
-            >
-                {tools.map((tool) => (
-                    <Tool key={tool.id} {...tool} />
-                ))}
-            </ul>
+            {tools ? (
+                <ul
+                    className={`${styles.skill__tools} ${
+                        column && styles["skill__tools--column"]
+                    }`}
+                >
+                    {tools.map((tool) => (
+                        <Tool key={tool.id} {...tool} />
+                    ))}
+                </ul>
+            ) : (
+                <ul className={styles.skill__skills}>
+                    {skills.map((skill, index) => (
+                        <li key={index}>{skill}</li>
+                    ))}
+                </ul>
+            )}
         </article>
     );
 };
--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import styles from "./Project.module.scss";
 import projects from "../../data/projects.json";
 import About from "../../components/About";
+import Skill from "../../components/Skill";
 
 const Project = () => {
     const { id } = useParams();
@@ -17,22 +18,20 @@ const Project = () => {
 
     return (
         project && (
-            <div key={project.id} className={styles.project}>
+            <section key={project.id} className={styles.project}>
                 <About project {...project} />
-                <section className={styles.project__section}>
-                    <div className={styles.project__top}>
-                        <div className={styles.project__introduction}>
-                            <h2 className={styles.project__title}>
-                                {project.logo}
-                            </h2>
-                            <p className={styles.project__text}>
-                                {project.description}
-                            </p>
-                        </div>
+                <div className={styles.project__left}>
+                    <div className={styles.project__introduction}>
+                        <h2 className={styles.project__title}>
+                            {project.logo}
+                        </h2>
+                        <p className={styles.project__text}>
+                            {project.description}
+                        </p>
                     </div>
-                    <div className={styles.project__bottom}></div>
-                </section>
-            </div>
+                    <Skill title="Compétences" skills={project.skills} />
+                </div>
+            </section>
         )
     );
 };
