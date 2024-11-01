70f7c4797aceef89221a0c2aae4f9e7abbf31300
David Horès <hores.d@gmail.com>
2024-11-01 01:27:28 +0100

    Add linkedin, github, envelope, globe, person-chalkboard, file-pdf and table icons to Links and update projects data and Links style

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,21 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+    faEnvelope,
+    faGlobe,
+    faPersonChalkboard,
+    faFilePdf,
+    faTable
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./Links.module.scss";
+
+const icon = {
+    faLinkedin,
+    faGithub,
+    faEnvelope,
+    faGlobe,
+    faPersonChalkboard,
+    faFilePdf,
+    faTable
+};
 
 const Links = ({
     links = [
         {
             title: "David Horès",
             url: "https://www.linkedin.com/in/dhores/",
-            logo: "Logo"
+            logo: "faLinkedin"
         },
         {
             title: "Dave25440",
             url: "https://github.com/Dave25440",
-            logo: "Logo"
+            logo: "faGithub"
         },
         {
             title: "hores.d@gmail.com",
             url: "mailto:hores.d@gmail.com",
-            logo: "Logo"
+            logo: "faEnvelope"
         }
     ]
 }) => {
@@ -24,6 +43,12 @@ const Links = ({
             {links.map((link, index) => (
                 <li key={index}>
                     <a href={link.url} className={styles.links__link}>
+                        <FontAwesomeIcon
+                            icon={icon[link.logo]}
+                            size="xl"
+                            fixedWidth
+                            className={styles.links__icon}
+                        />
                         {link.title}
                     </a>
                 </li>
