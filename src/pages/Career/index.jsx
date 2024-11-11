00f342a77e1da46b3fa1668c10997d3c345bf5d4
David Horès <hores.d@gmail.com>
2024-11-11 17:34:20 +0100

    Replace p tag with ul and add content to Career and update Career style

--- src/pages/Career/index.jsx
+++ src/pages/Career/index.jsx
@@ -9,16 +9,79 @@ const Career = () => {
     return (
         <section className={styles.career}>
             <h2 className={styles.career__title}>Parcours</h2>
-            <p className={styles.career__text}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-                sed luctus lacus. Etiam porta consequat nulla, vel aliquet quam
-                pulvinar sed. Proin justo massa, ultrices in augue quis, viverra
-                varius purus. Etiam non commodo dolor. Aliquam hendrerit
-                porttitor est vitae gravida. Vestibulum urna libero, placerat a
-                tellus eget, maximus commodo quam. Morbi non justo odio.
-                Suspendisse egestas tincidunt libero, vitae maximus mauris
-                vestibulum vitae.
-            </p>
+            <ul className={styles.career__list}>
+                <li className={styles["career__list--title"]}>
+                    <h3 className={styles.career__subtitle}>Expérience</h3>
+                </li>
+                <li>2018-2024</li>
+                <li>
+                    <strong>Journaliste Reporter d’Images</strong> - Mosaïk
+                    Cristal, Bitche et Sarreguemines
+                </li>
+                <li>2012-2018</li>
+                <li>
+                    <strong>
+                        Responsable du pôle magazine et divertissement
+                    </strong>{" "}
+                    - TV Cristal, Bitche
+                </li>
+                <li>2010-2012</li>
+                <li>
+                    <strong>Journaliste Reporter d’Images</strong> - TV Cristal,
+                    Bitche
+                </li>
+            </ul>
+            <ul className={styles.career__list}>
+                <li className={styles["career__list--title"]}>
+                    <h3
+                        className={`${styles.career__subtitle} ${styles["career__subtitle--secondary"]}`}
+                    >
+                        Formation
+                    </h3>
+                </li>
+                <li>2024</li>
+                <li>
+                    <strong>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://openclassrooms.com/fr/paths/899-developpeur-web"
+                            className={styles.career__link}
+                        >
+                            Développeur Web
+                        </a>
+                    </strong>{" "}
+                    - OpenClassrooms, Paris (distanciel)
+                    <br />
+                    Certification professionnelle :{" "}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.francecompetences.fr/recherche/rncp/38145/"
+                        className={styles.career__link}
+                    >
+                        Développeur informatique
+                    </a>
+                </li>
+                <li>2018</li>
+                <li>
+                    <strong>
+                        Comité d'Hygiène, de Sécurité et des Conditions de
+                        Travail
+                    </strong>{" "}
+                    - Communauté de Communes du Pays de Bitche
+                </li>
+                <li>2013</li>
+                <li>
+                    <strong>Filmer avec un reflex numérique</strong> - Centre de
+                    Formation et de Perfectionnement des Journalistes, Paris
+                </li>
+                <li>2008-2010</li>
+                <li>
+                    <strong>DUT Services et Réseaux de Communication</strong> -
+                    IUT de Haguenau
+                </li>
+            </ul>
         </section>
     );
 };
